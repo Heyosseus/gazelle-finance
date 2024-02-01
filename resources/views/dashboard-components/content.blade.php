9960b325603669b8dd972b4d51077586a1ea0464
heyosseus <ratiruxadzee@gmail.com>
2024-02-01 20:16:24 +0400

    refactor: extract content from home blade to component

---
+++ resources/views/dashboard-components/content.blade.php
@@ -0,0 +1,39 @@
+<div class="flex lg:flex-col xl:flex-row p-4 lg:p-20 justify-between xl:space-x-4">
+    <div class="block lg:w-full  xl:w-[700px] lg:h-[400px] rounded-md bg-amber-950"></div>
+
+    <div class="w-full xl:w-[700px] px-4 py-8 lg:p-10 rounded-lg shadow-xl flex flex-col justify-between ">
+        <h1 class="text-2xl lg:text-3xl  lg:text-center xl:text-start [text-shadow:_0_1px_0_rgb(0_0_0_/_60%)]">
+            Partnering for
+            business <span class="text-pink-950 font-bold">Growth</span></h1>
+
+        <p class="mt-5  lg:mt-8">We invest in gazelles, high-growth, small and medium companies <span
+                class="text-pink-950 font-bold">(SMEs)</span>
+            in the Eurasia region, with a current focus on <em class=" [text-shadow:_0_1px_0_rgb(0_0_0_/_60%)]">Georgia</em>
+            and <em class=" [text-shadow:_0_1px_0_rgb(0_0_0_/_60%)]">Armenia.</em>
+            <br>
+            <br>
+            We provide growth capital and businesses development services to our <em
+                class=" [text-shadow:_0_1px_0_rgb(0_0_0_/_60%)]">portfolio</em> companies
+            to rapidly scale their businesses and become market leaders.</p>
+
+        <div
+            class="flex flex-col lg:flex-row space-y-2 lg:space-y-0 text-center flex-wrap mt-5 lg:mt-10  lg:justify-center xl:justify-start lg:space-x-4">
+            <div
+                class="border py-2 px-4 rounded-full border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white transition-colors">
+                Growth
+            </div>
+            <div
+                class="border py-2 px-4 rounded-full border-pink-950 text-pink-950 hover:bg-pink-950 hover:text-white transition-colors">
+                SMEs
+            </div>
+            <div
+                class="border py-2 px-4 rounded-full border-orange-700 text-orange-700 hover:bg-orange-700 hover:text-white transition-colors">
+                Services
+            </div>
+            <div
+                class="border py-2 px-4 rounded-full border-green-600 text-green-600 hover:bg-green-600 hover:text-white transition-colors">
+                Portfolio
+            </div>
+        </div>
+    </div>
+</div>
