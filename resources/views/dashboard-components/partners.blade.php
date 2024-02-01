0c62d8978c36efab555e9712bc33346ed98c4423
heyosseus <ratiruxadzee@gmail.com>
2024-02-01 17:48:20 +0400

    feat: add assets and extract dashboard components

---
+++ resources/views/dashboard-components/partners.blade.php
@@ -0,0 +1,7 @@
+<div class="flex justify-around items-start lg:px-20 h-screen">
+    <div class="flex flex-col items-center ">
+        <h1 class="text-3xl border-b-2 py-2 border-pink-950">Our <span class="text-pink-950 font-bold">Partners</span> </h1>
+    </div>
+    <div class="block lg:w-full  xl:w-[700px] lg:h-[400px] rounded-md bg-amber-950"></div>
+
+</div>
