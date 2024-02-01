6967dbe5fe45bf9ab40de092e783e6e8efa10454
heyosseus <ratiruxadzee@gmail.com>
2024-02-01 23:00:43 +0400

    feat: create about and products pages

--- resources/views/components/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <div class="bg-gray-950 py-20 ">
-        <p class="text-center text-white">© 2024, Gazelle Finance, All rights reserved</p>
-    </div>
-</div>
-
---
+++ resources/views/components/home-footer.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <div class="bg-gray-950 py-20">
+        <p class="text-center text-white">© 2024, Gazelle Finance, All rights reserved</p>
+    </div>
+</div>
+
