d4b77eefed5890e8bb1c57fdccce0ca90f1c8155
heyosseus <ratiruxadzee@gmail.com>
2024-02-07 23:20:35 +0400

    feat: update layouts and add infinite scroll for images

--- resources/views/dashboard-components/partners.blade.php
+++ resources/views/dashboard-components/partners.blade.php
@@ -1,9 +1,9 @@
-<div class=" flex justify-around items-center lg:p-20 mt-20" id="partners">
-    <div class="flex flex-col items-center lg:items-start">
+<div class="flex flex-col w-full lg:flex-row justify-around items-center px-4 space-x-4 lg:p-20 mt-20" id="partners">
+    <div class="flex flex-col lg:w-1/2 items-center lg:items-start">
         <h1 class="text-3xl  lg:text-5xl border-b-2 py-2 border-pink-950 ">Our <span class="text-pink-950 font-bold">Partners</span>
         </h1>
-        <p class="mt-10 p-4 lg:p-0 text-center lg:text-start w-fit lg:w-[600px] leading-8"><span
-                class="text-pink-950 font-semibold"> Empowering </span> success through strategic partnerships, we
+        <p class="mt-10 text-lg text-center lg:text-start w-full xl:w-[500px] leading-8"><span
+                class="text-pink-950 font-semibold "> Empowering </span> success through strategic partnerships, we
             collaborate to drive innovation and achieve shared
             goals. </p>
         <a href="#" class="text-pink-950 font-semibold mt-20 flex items-center space-x-2">
@@ -11,21 +11,65 @@
             <img src="{{asset('icons/longRightArrow.svg')}}" alt="" width="15">
         </a>
     </div>
-    <div class="hidden lg:block lg:w-full  xl:w-[700px] lg:h-[400px] rounded-md bg-amber-950"></div>
+
+    <img src="{{asset('assets/community/gazelle-partners.png')}}" alt="" class="flex w-full lg:w-1/2 mt-10 xl:mt-0  h-auto rounded-md shadow-xl lg:ml-0 lg:mr-10">
 </div>
 <div
-    class="bg-gray-100 p-4 mt-20 lg:px-20 flex-col lg:flex lg:flex-row lg:justify-evenly items-center border-t-2 border-b-2 border-gray-200">
-    <div class="flex items-center justify-evenly w-full">
-        <div class="w-40"><img src="{{asset('logos/nout.png')}}" alt=""></div>
-        <div class="w-28 "><img src="{{asset('logos/larar.png')}}" alt=""></div>
-        <div class="w-40"><img src="{{asset('logos/sonati.png')}}" alt=""></div>
+    class="carousel-container bg-gray-100 p-4 mt-20 lg:px-20 flex-col lg:flex lg:flex-row lg:justify-between items-center border-t-2 border-b-2 border-gray-200">
+    <div class="flex items-center justify-between w-full carousel-track">
+        <div class="w-40 carousel-card"><img src="{{asset('logos/nout.png')}}" alt=""></div>
+        <div class="w-28 carousel-card "><img src="{{asset('logos/larar.png')}}" alt=""></div>
+        <div class="w-40 carousel-card"><img src="{{asset('logos/sonati.png')}}" alt=""></div>
+        <div class="w-32 carousel-card"><img src="{{asset('logos/apaga.jpg')}}" alt=""></div>
+        <div class="w-36 carousel-card"><img src="{{asset('logos/gp.png')}}" alt=""></div>
+        <div class="w-28 carousel-card"><img src="{{asset('logos/inpreso.png')}}" alt=""></div>
     </div>
-
-    <div class="flex items-center justify-evenly w-full">
-        <div class="w-32"><img src="{{asset('logos/apaga.jpg')}}" alt=""></div>
-        <div class="w-36"><img src="{{asset('logos/gp.png')}}" alt=""></div>
-        <div class="w-28"><img src="{{asset('logos/inpreso.png')}}" alt=""></div>
-    </div>
-
 </div>
+<style>
+    .carousel-container {
+        display: flex;
+        justify-content: space-between;
+        overflow: hidden;
+        position: relative;
+        padding: 60px;
+        border-color: #4b5563;
+        width: 100%;
+    }
+
+    .carousel-card {
+        display: flex;
+        justify-content: space-between;
+        &:hover {
+            transform: scale(0.9);
+            box-shadow: 5px 5px 8px rgba(222, 222, 222, 0.362),
+            10px 10px 8px rgba(152, 133, 133, 0.392),
+            15px 15px 8px rgba(139, 154, 139, 0.303);
+        }
+        border-radius: 20%;
+    }
+
+
+    .carousel-track {
+        display: flex;
+        position: absolute;
+        left: 0;
+        justify-content: space-around;
+        align-items: center;
+        gap: 16px;
+        width: 100%;
+        animation: slide 20s linear infinite backwards;
+        &:hover {
+            animation-play-state: paused;
+        }
+    }
+
+    @keyframes slide {
+        0% {
+            transform: translateX(calc(100% + 16px));
+        }
+        100% {
+            transform: translateX(-100%);
+        }
+    }
+</style>
 
--- resources/views/dashboard-components/portfolio.blade.php
+++ resources/views/dashboard-components/portfolio.blade.php
@@ -1,59 +1,107 @@
-<div id="portfolio" class="flex flex-col justify-between h-full px-4 mt-20 lg:px-20 xl:px-32 ">
-    <div class="flex lg:w-full mb-20 justify-center items-center">
-        <h1 class="text-3xl lg:text-5xl border-b-2 py-2 w-fit border-pink-950 text-center">Our <span class="text-pink-950 font-bold">Gazelles</span></h1>
+<div id="portfolio" class="flex flex-col justify-between h-full px-4 mt-32 lg:px-16 ">
+    <div class="flex lg:w-full mb-10 justify-center items-center">
+        <h1 class="text-3xl lg:text-5xl border-b-2 py-2 w-fit border-pink-950 text-center">Our <span
+                class="text-pink-950 font-bold">Gazelles</span></h1>
     </div>
 
-    <div class="flex items-center justify-between xl:mt-10">
-        <div class="hidden lg:flex flex-col items-start">
-            <h1 class="text-2xl">If you're unfamiliar with <span class="text-pink-950 font-bold">Gazelles</span> </h1>
-            <button id="showInfo" class="rounded-full px-5 py-2 border border-pink-950 mt-4 bg-white text-pink-950 hover:bg-pink-950 hover:text-white hover:border-white" onclick="bounceAnimation()">Read</button>
+    <div class="flex flex-col xl:flex-row items-end space-x-4 justify-around xl:mt-10">
+        <div class="flex flex-col space-y-8">
+            <div class="hidden lg:flex flex-col items-start">
+                <h1 class="text-2xl">If you're unfamiliar with <span class="text-pink-950 font-bold">Gazelles</span></h1>
+            </div>
+            <div id="info"
+                 class=" w-full  bg-gray-950 text-white text-md  xl:w-[600px] px-6 py-10 lg:py-6 lg:pb-12 lg:px-10 rounded-lg shadow-2xl flex-col justify-start">
+                <div>
+                    <div><span class="text-white font-bold text-2xl">Gazelle</span> - nimble, fast, ability to accelerate quickly.
+                    </div>
+                    <br>
+                    Gazelles are fast growing small and medium sized enterprises (<em>SMEs</em>) that have the potential to
+                    accelerate sales by 20 percent or more per year for more than a four year period.
+                    <br>
+                    <br>
+                    Gazelles constitute only 5-10 percent of all private companies, yet in typical years they create 50-80
+                    percent of all net new jobs.
+                </div>
+            </div>
         </div>
 
-        <div id="info" class=" w-full mx-8  text-md  xl:w-[700px] px-6 py-10 lg:p-12 rounded-lg shadow-2xl flex-col justify-start">
-            <p>
-                <div> <span class="text-pink-950 font-bold ">Gazelle</span> - nimble, fast, ability to accelerate quickly.</div>
-                <br>
+        <div class="w-full xl:w-1/2 px-4 p-10 rounded-lg shadow-xl xl:text-sm 2xl:text-lg flex flex-col justify-between ">
+            <h1 class="text-xl lg:text-3xl  lg:text-center  [text-shadow:_0_1px_0_rgb(0_0_0_/_60%)]">
+                Building fast growing
+                 <span class="text-pink-950 font-bold">Companies</span></h1>
 
-                Gazelles are fast growing small and medium sized enterprises (<em>SMEs</em>)  that have the potential to accelerate sales by 20 percent or more per year for more than a four year period.
+            <p class="mt-5 px-4  lg:mt-8">We invest in gazelles, high-growth, small and medium companies <span
+                    class="text-pink-950 font-bold">(SMEs)</span>
+                in the Eurasia region, with a current focus on <em class=" [text-shadow:_0_1px_0_rgb(0_0_0_/_60%)]">Georgia</em>
+                and <em class=" [text-shadow:_0_1px_0_rgb(0_0_0_/_60%)]">Armenia.</em>
                 <br>
                 <br>
-                Gazelles constitute only 5-10 percent of all private companies, yet in typical years they create 50-80 percent of all net new jobs.
-            </p>
+                We provide growth capital and businesses development services to our <em
+                    class=" [text-shadow:_0_1px_0_rgb(0_0_0_/_60%)]">portfolio</em> companies
+                to rapidly scale their businesses and become market leaders.</p>
+
+            <div
+                class="flex flex-col lg:flex-row space-y-2 lg:space-y-0 text-center flex-wrap mt-5 lg:mt-10  lg:justify-center lg:space-x-4">
+                <div
+                    class="border py-2 cursor-pointer  px-4 rounded-full border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white transition-colors">
+                    Growth
+                </div>
+                <div
+                    class="border py-2 cursor-pointer  px-4 rounded-full border-pink-950 text-pink-950 hover:bg-pink-950 hover:text-white transition-colors">
+                    SMEs
+                </div>
+                <div
+                    class="border py-2  cursor-pointer px-4 rounded-full border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white transition-colors">
+                    Services
+                </div>
+                <div
+                    class="border py-2 cursor-pointer  px-4 rounded-full border-green-600 text-green-600 hover:bg-green-600 hover:text-white transition-colors">
+                    Portfolio
+                </div>
+                <div
+                    class="border py-2  cursor-pointer px-4 rounded-full border-red-700 text-red-700  hover:bg-red-700  hover:text-white transition-colors">
+                    Partners
+                </div>
+            </div>
         </div>
-
 
     </div>
+
+
     <div class="mt-20">
-        <h1 class="rounded-full px-5 w-fit flex mx-auto py-2 border mt-4 bg-pink-950 text-white hover:bg-white hover:text-pink-950 hover:border-pink-950 cursor-pointer">See More</h1>
+        <h1 class="rounded-full px-5 w-fit flex mx-auto py-2 border mt-4 bg-pink-950 text-white hover:bg-white hover:text-pink-950 hover:border-pink-950 cursor-pointer">
+            See More</h1>
     </div>
 
 
 </div>
 <div class="w-full h-px bg-gray-300 mt-10"></div>
 
-<script>
-    function bounceAnimation() {
-        const button = document.getElementById('info');
-        button.classList.add('bounce-animate');
-        button.addEventListener('animationend', () => {
-            button.classList.remove('bounce-animate');
-        }, { once: true });
-    }
-</script>
-<style>
-    @keyframes bounce {
-        0%, 20%, 50%, 80%, 100% {
-            transform: translateY(0);
-        }
-        40% {
-            transform: translateY(-40px) scale(1.1);
-        }
-        60% {
-            transform: translateY(-15px) scale(1.05);
-        }
-    }
+{{--<script>--}}
+{{--    function bounceAnimation() {--}}
+{{--        const info = document.getElementById('info');--}}
+{{--        info.classList.add('bounce-animate');--}}
+{{--        info.addEventListener('animationend', () => {--}}
+{{--            info.classList.remove('bounce-animate');--}}
+{{--        }, {once: true});--}}
+{{--    }--}}
 
-    .bounce-animate {
-        animation: bounce 1s ease-in-out;
-    }
-</style>
+{{--    setInterval(bounceAnimation, 30000)--}}
+{{--</script>--}}
+{{--<style>--}}
+{{--    @keyframes bounce {--}}
+{{--        0%, 20%, 50%, 80%, 100% {--}}
+{{--            transform: translateY(0);--}}
+{{--        }--}}
+{{--        40% {--}}
+{{--            transform: translateY(-40px) scale(1.1);--}}
+{{--        }--}}
+{{--        60% {--}}
+{{--            transform: translateY(-15px) scale(1.05);--}}
+{{--        }--}}
+{{--    }--}}
+
+{{--    .bounce-animate {--}}
+{{--        animation: bounce 1s ease-in-out;--}}
+{{--    }--}}
+{{--</style>--}}
