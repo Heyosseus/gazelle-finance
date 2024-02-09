c6d28fd4648c38e35cbf137acffbb822ecf71d84
Wishhh <agagosha58@gmail.com>
2024-02-09 17:14:20 +0400

    generate migrations , models and controllers

---
+++ database/migrations/2024_02_09_130125_create_ukraine_bridge_facility_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ukraine_bridge_facility', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ukraine_bridge_facility');
+    }
+};
---
+++ database/migrations/2024_02_09_130132_create_portfolio_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('portfolio', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('title')->nullable();
+            $table->text('description');
+            $table->string('company');
+            $table->string('industry');
+            $table->string('location');
+            $table->string('website_url');
+            $table->string('logo');
+            $table->string('image');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('portfolio');
+    }
+};
