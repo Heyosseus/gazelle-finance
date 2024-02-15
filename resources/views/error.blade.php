6372e334a7d5f02987be9ac06ad254d572f0b58c
heyosseus <ratiruxadzee@gmail.com>
2024-02-15 17:34:20 +0400

    feat: add content on about page

---
+++ resources/views/error.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <div class="bg-gray-100 h-screen flex flex-col items-center  justify-center ">
+        <h1 class="text-4xl tracking-wider">NOT FOUND</h1>
+        <a href="/" class="text-sm mt-4"> return to dashboard</a>
+    </div>
+</x-layout>
